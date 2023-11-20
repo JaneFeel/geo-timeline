cda99f8afa56fc52008b6de349a06749eb07b682
JaneFeel <rooffeel@gmail.com>
2023-11-20 16:54:40 +0800

    add dist to repo

---
+++ dist/helpers.d.ts
@@ -0,0 +1,3 @@
+import { Selection } from "d3";
+export declare function getTextWidth(text: string, font: string): any;
+export declare function trans<T extends Selection<any, any, any, any>>(node: T, duration: number): T;
---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import GeoTimeLine from "./GeoTimeLine";
+import GeoTimeScale from "./GeoTimeScale";
+export { GeoTimeLine, GeoTimeScale };
+export * from './typing';
